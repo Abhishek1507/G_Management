2c8545f87a2d4b6188fda53f216ec7a62d7a25c8
Abhishek <abhishek1507.gorivale@gmail.com>
2021-05-13 13:03:55 +0530

    Created ui design for login

--- app/src/main/java/com/example/g_management/activities/MainActivity.kt
+++ app/src/main/java/com/example/g_management/activities/MainActivity.kt
@@ -1,11 +1,17 @@
-package com.example.g_management
+package com.example.g_management.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.g_management.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Handler().postDelayed({
+            startActivity(Intent(this,Signup::class.java))
+        },5000)
     }
 }
